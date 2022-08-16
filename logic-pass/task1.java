2ad3860208d015dd18c09de3d4b762c1de9bfc41
Omar Yosuif Banna <62323493+omarYousifKamal@users.noreply.github.com>
2022-08-16 15:43:30 +0300

    Create task1.java

---
+++ logic-pass/task1.java
@@ -0,0 +1,23 @@
+
+//hi, here im using java to find the prime number in any giveng set of numbers.
+import java.io.*;
+class Scaler{
+   public static int isprime(int n)
+   {
+      for (int i = 2; i < n; i++){
+         if(n%i == 0){
+            return -1;
+         }
+      }
+      return 1;
+   }
+   public static void main(String args[])
+   {
+      int lower_bound = 2, upper_bound = 50;
+      for (int i = lower_bound; i <= upper_bound; i++)
+      {if(isprime(i) == 1)
+         {System.out.print(i+" ");
+         }
+      }
+   }
+}
