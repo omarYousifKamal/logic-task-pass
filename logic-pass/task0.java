bfc77b64a65015540a2c797d0a686cb7209ca9bb
Omar Yosuif Banna <62323493+omarYousifKamal@users.noreply.github.com>
2022-08-16 15:42:24 +0300

    Create task0.java

---
+++ logic-pass/task0.java
@@ -0,0 +1,31 @@
+//hi, im using java method to remove any given character and give a new meaning to a word after deleting one character using join() + list comprehension
+
+class Omar
+{
+static void removeChar(String s, char c)
+{
+	int j, count = 0, n = s.length();
+	char []t = s.toCharArray();
+	for (int i = j = 0; i < n; i++)
+	{
+		if (t[i] != c)
+		t[j++] = t[i];
+		else
+			count++;
+	}
+	
+	while(count > 0)
+	{
+		t[j++] = '\0';
+		count--;
+	}
+	
+	System.out.println(t);
+}
+public static void main(String[] args)
+{
+	String s = "fall";
+	removeChar(s, 'f');
+}
+}
+
